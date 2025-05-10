d05540acd24d645f2faa9cb1da39d900e297e1cc
Charles Taylor <charlestaylor95@gmail.com>
2025-05-10 13:06:41 -0400

    Compile angular to custom elements

--- angular/src/components/district/district.component.ts
+++ angular/src/components/district/district.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'district',
+  selector: 'app-district',
   imports: [],
-  templateUrl: './district.component.html'
+  templateUrl: './district.component.html',
 })
-export class DistrictComponent {
-
-}
+export class DistrictComponent {}
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,7 +1,38 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { AppComponent } from './components/app/app.component';
+import {
+  DoBootstrap,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { GameComponent } from './components/game/game.component';
+import { DistrictComponent } from './components/district/district.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideExperimentalZonelessChangeDetection()],
-}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [provideExperimentalZonelessChangeDetection()],
+// }).catch((err) => console.error(err));
+
+@NgModule({
+  imports: [BrowserModule, GameComponent, DistrictComponent],
+})
+class CustomElementsModule implements DoBootstrap {
+  constructor(injector: Injector) {
+    customElements.define(
+      'citadels-game',
+      createCustomElement(GameComponent, { injector }),
+    );
+
+    customElements.define(
+      'citadels-district',
+      createCustomElement(DistrictComponent, { injector }),
+    );
+  }
+
+  ngDoBootstrap() {} // Required to avoid auto-bootstrapping
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(CustomElementsModule, { ngZone: 'noop' })
+  .catch((err) => console.error(err));
