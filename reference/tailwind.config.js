eb493f899ff103a3f9d29e2a454275e0f2452a4c
Charles Taylor <charlestaylor95@gmail.com>
2025-05-11 12:07:11 -0400

    WIP

--- reference/tailwind.config.js
+++ reference/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   plugins: [require("daisyui")],
-  content:  [
-    "citadels_server/templates/**/*", 
-    "citadels_server/src/templates.rs", 
-    "citadels_server/src/templates/filters.rs"
+  content: [
+    "citadels_server/templates/**/*",
+    "citadels_server/src/templates.rs",
+    "citadels_server/src/templates/filters.rs",
   ],
   daisyui: {
     themes: true,
@@ -12,21 +12,21 @@ module.exports = {
   theme: {
     extend: {
       borderRadius: {
-        'box': 'var(--rounded-box, 1rem)',
+        box: "var(--rounded-box, 1rem)",
       },
       borderWidth: {
-        '3': '3px',
+        3: "3px",
       },
-       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
       colors: {
-        'suit-trade': "rgb(104, 189, 126)",
-        'suit-military': "rgb(223, 71, 71)",
-        'suit-religious': "rgb(75, 203, 214)",
-        'suit-noble':   "rgb(224, 189, 22)", 
-        'suit-unique': "rgb(169, 107, 244)",
-      }
-    }
+        "suit-trade": "rgb(104, 189, 126)",
+        "suit-military": "rgb(223, 71, 71)",
+        "suit-religious": "rgb(75, 203, 214)",
+        "suit-noble": "rgb(224, 189, 22)",
+        "suit-unique": "rgb(169, 107, 244)",
+      },
+    },
   },
-}
+};
