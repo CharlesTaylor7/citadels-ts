2d92c40ecba5d762d5329a02369c7e2b2f60688b
Charles Taylor <charlestaylor95@gmail.com>
2025-05-11 10:19:13 -0400

    Render all districts

--- angular/src/components/game/game.component.ts
+++ angular/src/components/game/game.component.ts
@@ -1,6 +1,7 @@
 import { GameService } from '@/services/game.service';
 import { Component } from '@angular/core';
-import { DistrictComponent } from '../district/district.component';
+import { DistrictComponent } from '@/components/district/district.component';
+import { DISTRICT_NAMES } from '@/core/districts';
 
 @Component({
   selector: 'app-game',
@@ -10,4 +11,6 @@ import { DistrictComponent } from '../district/district.component';
 })
 export class GameComponent {
   constructor(private gameService: GameService) {}
+
+  districts = DISTRICT_NAMES;
 }
